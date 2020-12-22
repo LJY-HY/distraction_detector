ea1653b4d082c51951fe52652cd6c275efc07d0c
LJY-HY <dlwodbs1421@gmail.com>
2020-12-22 13:55:12 +0900

    final

--- dataset/distractor.py
+++ dataset/distractor.py
@@ -21,13 +21,17 @@ def make_dataloader(args):
         transforms.Resize((224,224)),
         transforms.ToTensor(),
     ])
-    source_dataset = ImageFolder(root = '/home/esoc/datasets/driver_detection/imgs/train/', transform = transform_train)
-    s_train_dataset, s_test_dataset = random_split(source_dataset,[17424,5000])
+    source_dataset_train = ImageFolder(root = '/home/esoc/datasets/driver_detection/imgs/biased_train/source/train', transform = transform_train)
+    source_dataset_test = ImageFolder(root = '/home/esoc/datasets/driver_detection/imgs/biased_train/source/test', transform = transform_test)
+    target_dataset_train = ImageFolder(root = '/home/esoc/datasets/driver_detection/imgs/biased_train/target/train', transform = transform_train)
+    target_dataset_test = ImageFolder(root = '/home/esoc/datasets/driver_detection/imgs/biased_train/target/test', transform = transform_test)
 
-    s_train_dataloader = DataLoader(s_train_dataset, batch_size = args.batch_size, shuffle=True,num_workers=4)
-    s_test_dataloader = DataLoader(s_test_dataset, batch_size = args.batch_size, shuffle=True,num_workers=4)
+    source_train_dataloader = DataLoader(source_dataset_train, batch_size = args.batch_size, shuffle=True,num_workers=4)
+    source_test_dataloader = DataLoader(source_dataset_test, batch_size = args.batch_size, shuffle=True,num_workers=4)
+    target_train_dataloader = DataLoader(target_dataset_train, batch_size = args.batch_size, shuffle=True,num_workers=4)
+    target_test_dataloader = DataLoader(target_dataset_test, batch_size = args.batch_size, shuffle=True,num_workers=4)
 
-    return s_train_dataloader, s_test_dataloader
+    return source_train_dataloader, source_test_dataloader, target_train_dataloader, target_test_dataloader
 
 def make_biased_dataloader(args):
     #split subject according to args.source
@@ -52,9 +56,8 @@ def make_biased_dataloader(args):
     if 'subject' in target_subject_train:
         target_subject_train.remove('subject')
 
-    source_subject_test.append(source_subject_train.pop(np.random.randint(len(source_subject_train))))
+    source_subject_test.append(source_subject_train.pop(1)) #p015
     target_subject_test.append(target_subject_train.pop(np.random.randint(len(target_subject_train))))
-
     source_img_train = []
     source_label_train = []
     target_img_train = []
@@ -95,6 +98,7 @@ def make_biased_dataloader(args):
 
     source_imsave(source_img_train,source_label_train,source_img_test,source_label_test)
     target_imsave(target_img_train,target_label_train,target_img_test,target_label_test)
+
     source_dataset_train = ImageFolder(root = '/home/esoc/datasets/driver_detection/imgs/biased_train/source/train', transform = transform_train)
     source_dataset_test = ImageFolder(root = '/home/esoc/datasets/driver_detection/imgs/biased_train/source/test', transform = transform_test)
     target_dataset_train = ImageFolder(root = '/home/esoc/datasets/driver_detection/imgs/biased_train/target/train', transform = transform_train)
@@ -104,7 +108,6 @@ def make_biased_dataloader(args):
     source_test_dataloader = DataLoader(source_dataset_test, batch_size = args.batch_size, shuffle=True,num_workers=4)
     target_train_dataloader = DataLoader(target_dataset_train, batch_size = args.batch_size, shuffle=True,num_workers=4)
     target_test_dataloader = DataLoader(target_dataset_test, batch_size = args.batch_size, shuffle=True,num_workers=4)
-
     return source_train_dataloader, source_test_dataloader, target_train_dataloader, target_test_dataloader
 
 def source_imsave(source_img_train,source_label_train,source_img_test,source_label_test):
@@ -116,26 +119,26 @@ def source_imsave(source_img_train,source_label_train,source_img_test,source_lab
         shutil.rmtree(path+'target/')
 
     for count in range(len(source_img_train)):
-        if not os.path.exists(path+'source/train/'+source_label_train[count]):
-            os.makedirs(path+'source/train/'+source_label_train[count])
-        shutil.copy(path+source_img_train[count], path+'source/train/'+source_label_train[count])
+        if not os.path.exists(path+'source/train/'+source_label_train[count][1]):
+            os.makedirs(path+'source/train/'+source_label_train[count][1])
+        shutil.copy(path+source_img_train[count], path+'source/train/'+source_label_train[count][1])
     
     for count in range(len(source_img_test)):
-        if not os.path.exists(path+'source/test/'+source_label_test[count]):
-            os.makedirs(path+'source/test/'+source_label_test[count])
-        shutil.copy(path+source_img_test[count], path+'source/test/'+source_label_test[count])
+        if not os.path.exists(path+'source/test/'+source_label_test[count][1]):
+            os.makedirs(path+'source/test/'+source_label_test[count][1])
+        shutil.copy(path+source_img_test[count], path+'source/test/'+source_label_test[count][1])
 
 def target_imsave(target_img_train,target_label_train,target_img_test,target_label_test):
     path = '/home/esoc/datasets/driver_detection/imgs/biased_train/'
     for count in range(len(target_img_train)):
-        if not os.path.exists(path+'target/train/'+target_label_train[count]):
-            os.makedirs(path+'target/train/'+target_label_train[count])
-        shutil.copy(path+target_img_train[count], path+'target/train/'+target_label_train[count])
+        if not os.path.exists(path+'target/train/'+target_label_train[count][1]):
+            os.makedirs(path+'target/train/'+target_label_train[count][1])
+        shutil.copy(path+target_img_train[count], path+'target/train/'+target_label_train[count][1])
     
     for count in range(len(target_img_test)):
-        if not os.path.exists(path+'target/test/'+target_label_test[count]):
-            os.makedirs(path+'target/test/'+target_label_test[count])
-        shutil.copy(path+target_img_test[count], path+'target/test/'+target_label_test[count])
+        if not os.path.exists(path+'target/test/'+target_label_test[count][1]):
+            os.makedirs(path+'target/test/'+target_label_test[count][1])
+        shutil.copy(path+target_img_test[count], path+'target/test/'+target_label_test[count][1])
 class ConcatDataset(torch.utils.data.Dataset):
     def __init__(self, *datasets):
         self.datasets = datasets
--- main.py
+++ main.py
@@ -18,113 +18,178 @@ def main():
     args.device = torch.device('cuda',args.gpu_id)
 
     #dataset setting
-    if args.source == None:
-        s_train_dataloader, s_test_dataloader = make_dataloader(args)
-        t_train_dataloader = s_train_dataloader
-        t_test_dataloader = s_test_dataloader
-    else:
+    if args.resume == 1:
         s_train_dataloader, s_test_dataloader, t_train_dataloader, t_test_dataloader = make_biased_dataloader(args)
+    elif args.resume == 0:
+        s_train_dataloader, s_test_dataloader, t_train_dataloader, t_test_dataloader = make_biased_dataloader(args)
+
+    concat_dataset = ConcatDataset(
+        s_train_dataloader.dataset,
+        t_train_dataloader.dataset
+    )
+    concat_dataset_test = ConcatDataset(
+        s_test_dataloader.dataset,
+        t_test_dataloader.dataset
+    )
+    train_loader = DataLoader(
+        concat_dataset,
+        batch_size = args.batch_size,
+        shuffle = True,
+        num_workers = 4
+    )
+    test_loader = DataLoader(
+        concat_dataset_test,
+        batch_size = args.batch_size,
+        shuffle = True,
+        num_workers = 4
+    )
+
     net = resnet.ResNet34(num_classes=10).to(args.device)
-    optimizer = optim.Adam(net.parameters(), lr=args.lr, weight_decay=args.wdecay)
-    # optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=args.wdecay)
+    optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=args.wdecay)
     scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer,T_max=20)
 
     if args.resume == 1:
-        checkpoint = torch.load('./checkpoint/'+'RN')
+        checkpoint = torch.load('./checkpoint/Source_Only/'+'RN_'+args.train_mode+'_'+'85')
         net.load_state_dict(checkpoint)
 
-    CE_loss = nn.CrossEntropyLoss()
-
-    path = './checkpoint/'+'RN'
-
+    path = './checkpoint/Source_Only/'+'RN_'+args.train_mode+'_'
     best_acc=0
     for epoch in range(args.epochs):
-        train(args, net, s_train_dataloader, t_train_dataloader, optimizer, CE_loss,scheduler, epoch)
-        acc = test(args, net, s_test_dataloader, t_test_dataloader, optimizer, CE_loss,scheduler, epoch)
+        train(args, net, train_loader, optimizer, scheduler, epoch)
+        acc = test(args, net, test_loader, optimizer, scheduler, epoch)
         scheduler.step()
         if best_acc<acc:
-            if not os.path.isdir('checkpoint/'):
-                os.makedirs('checkpoint/')
-            torch.save(net.state_dict(), path)
+            if not os.path.isdir('checkpoint/Source_Only/'):
+                os.makedirs('checkpoint/Source_Only/')
+            torch.save(net.state_dict(), path+str(epoch))
+            # best_acc = acc/ just save everything
 
-def train(args,net,s_train_dataloader,t_train_dataloader, optimizer,CE_loss,scheduler,epoch):
+def train(args,net,loader, optimizer, scheduler,epoch):
     net.train()
     train_loss = 0
-    p_bar = tqdm(range(s_train_dataloader.__len__()))
+    p_bar = tqdm(range(loader.__len__()))
     loss_average = 0
-    for batch_idx, (inputs, targets) in enumerate(s_train_dataloader):
-        inputs, targets = inputs.to(args.device), targets.to(args.device)     
-        optimizer.zero_grad()
+    
+    for batch_idx, (s_batch, t_batch) in enumerate(loader):
+        s_data, s_label = s_batch
+        t_data, t_label = t_batch
+        s_data = s_data.to(args.device)
+        s_label = s_label.to(args.device)
+        t_data = t_data.to(args.device)
+        t_label = t_label.to(args.device) 
+        
+        p = float(batch_idx + epoch * loader.__len__()) / args.epochs / loader.__len__()
+        alpha = 2. / (1. + np.exp(-10 * p)) - 1
+        alpha = alpha*0.2
+         
+        # Source Loss
+        s_class_output, s_domain_output = net(s_data,alpha=alpha)
+        s_class_loss = F.cross_entropy(s_class_output, s_label)
+        s_domain_label = torch.zeros(s_domain_output.shape[0]).long().to(s_domain_output.device)
+        s_domain_loss = F.cross_entropy(s_domain_output,s_domain_label)
 
-        outputs,domain_outputs = net(inputs)
-        loss = CE_loss(outputs,targets)
+        # Target Loss
+        t_class_output, t_domain_output = net(t_data,alpha=alpha)
+        t_class_loss = F.cross_entropy(t_class_output, t_label)
+        t_domain_label = torch.ones(t_domain_output.shape[0]).long().to(t_domain_output.device)
+        t_domain_loss = F.cross_entropy(t_domain_output,t_domain_label)
+
+        if args.train_mode=='SO':
+            loss = s_class_loss
+        elif args.train_mode=='CC':
+            loss = s_class_loss + t_class_loss
+        elif args.train_mode=='DA':
+            loss = s_class_loss + 0.2*(s_domain_loss + t_domain_loss)
+        elif args.train_mode=='TO':
+            loss = t_class_loss
+
+        optimizer.zero_grad()
         loss.backward()
         optimizer.step()
         train_loss += loss.item()
-        p_bar.set_description("Train Epoch: {epoch}/{epochs:2}. Iter: {batch:4}/{iter:4}. LR: {lr:.6f}. ce_loss: {ce_loss:.4f}. loss: {loss:.4f}.".format(
+        p_bar.set_description("Train Epoch: {epoch}/{epochs:2}. Iter: {batch:4}/{iter:4}. LR: {lr:.6f}. class_loss: {class_loss:.4f}. domain_loss: {domain_loss:.4f}. loss: {loss:.4f}.".format(
                     epoch=epoch + 1,
                     epochs=args.epochs,
                     batch=batch_idx + 1,
-                    iter=s_train_dataloader.__len__(),
+                    iter=loader.__len__(),
                     lr=scheduler.get_last_lr()[0],
-                    ce_loss = loss,
+                    class_loss = s_class_loss,
+                    domain_loss = s_domain_loss+t_domain_loss,
                     loss = train_loss/(batch_idx+1))
                     )
         p_bar.update()
     p_bar.close()
-    return train_loss/s_train_dataloader.__len__()        # average train_loss
+    return train_loss/loader.__len__()        # average train_loss
 
-def test(args, net, s_test_dataloader, t_test_dataloader, optimizer, CE_loss, scheduler, epoch):
+def test(args, net, loader, optimizer, scheduler, epoch):
     net.eval()
     test_loss = 0
-    acc = 0.
-    p_bar = tqdm(range(s_test_dataloader.__len__()))
-    with torch.no_grad():
-        for batch_idx, (inputs, targets) in enumerate(s_test_dataloader):
-            inputs, targets = inputs.to(args.device), targets.to(args.device)
-            outputs,domain_outputs = net(inputs)
-            loss = F.cross_entropy(outputs, targets)
-            test_loss += loss.item()
-            p_bar.set_description("Test Epoch: {epoch}/{epochs:4}. Iter: {batch:4}/{iter:4}. LR: {lr:.6f}. S_Loss: {loss:.4f}.".format(
-                    epoch=1,
-                    epochs=1,
-                    batch=batch_idx + 1,
-                    iter=s_test_dataloader.__len__(),
-                    lr=scheduler.get_last_lr()[0],
-                    loss=test_loss/(batch_idx+1)))
-            p_bar.update()
-            acc+=sum(outputs.argmax(dim=1)==targets)
-    p_bar.close()
-
+    acc_s = 0.
     acc_t = 0.
+    p_bar = tqdm(range(loader.__len__()))
+    test_class_loss = 0
+    test_domain_loss = 0
     with torch.no_grad():
-        for batch_idx, (inputs, targets) in enumerate(t_test_dataloader):
-            inputs, targets = inputs.to(args.device), targets.to(args.device)
-            outputs, domain_outputs = net(inputs)
-            loss = F.cross_entropy(outputs, targets)
-            test_loss += loss.item()
-            p_bar.set_description("Test Epoch: {epoch}/{epochs:4}. Iter: {batch:4}/{iter:4}. LR: {lr:.6f}. T_Loss: {loss:.4f}.".format(
-                    epoch=1,
-                    epochs=1,
+        for batch_idx, (s_batch, t_batch) in enumerate(loader):
+            s_data, s_label = s_batch
+            t_data, t_label = t_batch
+            s_data = s_data.to(args.device)
+            s_label = s_label.to(args.device)
+            t_data = t_data.to(args.device)
+            t_label = t_label.to(args.device)
+
+            p = float(batch_idx + epoch * loader.__len__()) / args.epochs / loader.__len__()
+            alpha = 2. / (1. + np.exp(-10 * p)) - 1
+
+            # Source Loss
+            s_class_output, s_domain_output = net(s_data,alpha=alpha)
+            s_class_loss = F.cross_entropy(s_class_output, s_label)
+            s_domain_label = torch.zeros(s_domain_output.shape[0]).long().to(s_domain_output.device)
+            s_domain_loss = F.cross_entropy(s_domain_output,s_domain_label)
+
+            # Target Loss
+            t_class_output, t_domain_output = net(t_data,alpha=alpha)
+            t_class_loss = F.cross_entropy(t_class_output, t_label)
+            t_domain_label = torch.ones(t_domain_output.shape[0]).long().to(t_domain_output.device)
+            t_domain_loss = F.cross_entropy(t_domain_output,t_domain_label)
+
+            s_class_pred = s_class_output.argmax(dim=1,keepdim=True)
+            s_domain_pred = s_domain_output.argmax(dim=1,keepdim=True)
+            t_class_pred = t_class_output.argmax(dim=1,keepdim=True)
+            t_domain_pred = t_domain_output.argmax(dim=1,keepdim=True)
+
+            class_loss = s_class_loss + t_class_loss
+            domain_loss = s_domain_loss + t_domain_loss
+            test_class_loss += class_loss.item()
+            test_domain_loss += domain_loss.item()
+            s_class_correct = s_class_pred.eq(s_label.view_as(s_class_pred)).sum().item()
+            s_domain_correct = s_domain_pred.eq(s_domain_label.view_as(s_domain_pred)).sum().item()
+
+            t_class_correct = t_class_pred.eq(t_label.view_as(t_class_pred)).sum().item()
+            t_domain_correct = t_domain_pred.eq(t_domain_label.view_as(t_domain_pred)).sum().item()
+
+            p_bar.set_description("Test Epoch: {epoch}/{epochs:2}. Iter: {batch:4}/{iter:4}. LR: {lr:.6f}. class_loss: {class_loss:.4f}. domain_loss: {domain_loss:.4f}. loss: {loss:.4f}.".format(
+                    epoch=epoch + 1,
+                    epochs=args.epochs,
                     batch=batch_idx + 1,
-                    iter=s_test_dataloader.__len__(),
+                    iter=loader.__len__(),
                     lr=scheduler.get_last_lr()[0],
-                    loss=test_loss/(batch_idx+1)))
+                    class_loss = s_class_loss+t_class_loss,
+                    domain_loss = s_domain_loss+t_domain_loss,
+                    loss = (test_class_loss+test_domain_loss)/(batch_idx+1))
+                    )
             p_bar.update()
-            acc_t+=sum(outputs.argmax(dim=1)==targets)
-    p_bar.close()
-    
-    acc = acc.float()/s_test_dataloader.dataset.__len__()
-    acc_t = acc_t.float()/t_test_dataloader.dataset.__len__()
-    acc = acc*100
+            acc_s+=s_class_correct
+            acc_t+=t_class_correct
+    p_bar.close()    
+    acc_s = acc_s/loader.dataset.__len__()
+    acc_t = acc_t/loader.dataset.__len__()
+    acc_s = acc_s*100
     acc_t = acc_t*100
-    print('Source Domain Accuracy :'+ '%0.2f'%acc )
+    print('Source Domain Accuracy :'+ '%0.2f'%acc_s )
     print('Target Domain Accuracy :'+ '%0.2f'%acc_t)
 
-    return acc
+    return acc_s
 
 if __name__ == '__main__':
     main()
-
-
-# TODO : alpha 지정해줘야함
